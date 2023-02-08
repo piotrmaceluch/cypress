df3556a30203ed11e320d130a83fcd46cd277646
Harbacheuski Dima <dzmitry.harbacheuski@solwit.com>
2023-02-08 08:55:02 +0100

    added main and base pages, and changed baseUrl

---
+++ cypress/support/page_objects/basePage.js
@@ -0,0 +1,15 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
+  
+  export class BasePage {
+    acceptCookiesPl() {
+        cy.contains('Akceptuj').click()
+    }
+
+    acceptCookiesEn() {
+        cy.get('#cookie_action_accept').click()
+    }
+}
+export const basePage = new BasePage()
+
