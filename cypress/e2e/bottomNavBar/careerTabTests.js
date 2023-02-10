89f35eaedab54e4fb1d8dfbe83ada84cfe457f9f
Harbacheuski Dima <dzmitry.harbacheuski@solwit.com>
2023-02-10 12:50:39 +0100

    refactor tests

--- cypress/e2e/bottomNavBar/careerTabTests.js
+++ cypress/e2e/bottomNavBar/careerTabTests.js
@@ -29,16 +29,16 @@ describe('Testing careera tab', () => {
     onCareerPage.meetTheHrTeamShouldContainRightText()
   })
 
-  it('Tiles should contain right text',()=>{
-    onCareerPage.checkIfTileContainsRightText()
-    onCareerPage.availableTypesOfEmploymentTileShouldContainRightText()
-    onCareerPage.moneyBonusesTypesOfEmploymentTileShouldContainRightText()
-    onCareerPage.developmentOpportunitiesTypesOfEmploymentTileShouldContainRightText()
-    onCareerPage.youCanChooseAPathForYourselfTypesOfEmploymentTileShouldContainRightText()
-    onCareerPage.solwitIsDogFriendlyTileShouldContainRightText()
-    onCareerPage.freeLargeUndergroundCarParkingTileShouldContainRightText()
-    onCareerPage.forYourSafetyTileShouldContainRightText()
-    onCareerPage.weSupportYourPassionsTileShouldContainRightText()
+  it.only('Tiles should contain right text',()=>{
+    onCareerPage.checkIfTextIsVisible(`You work as you please: at home, at the office, or hybrid – the choice is yours!`);
+    onCareerPage.checkIfTextIsVisible(`Available types of employment: contract of employment or B2B-contracts`);
+    onCareerPage.checkIfTextIsVisible(`Money bonuses: for blog articles, recommendations of new employees, sales team support, conducting training, substantive support in the recruitment process`);
+    onCareerPage.checkIfTextIsVisible(`Development opportunities:internal training (Solwit Academy of Development), acting as an internal trainer, external training, certifications, language platform, e-learning training`);
+    onCareerPage.checkIfTextIsVisible(`You can choose a path for yourself – either an engineering or managerial one`);
+    onCareerPage.checkIfTextIsVisible(`Solwit is dog-friendly, but above all people-friendly, as confirmed by the AudIT survey for the 3rd year in a row`);
+    onCareerPage.checkIfTextIsVisible(`Free large underground car parking no need to book, no hassle`);
+    onCareerPage.checkIfTextIsVisible(`For your safety:we provide medical insurance (TU Zdrowie), life insurance, and for health - Multisport card`);
+    onCareerPage.checkIfTextIsVisible(`We support your passions (not only in terms of sports) we fund integrations and meetings (company, team, pro-health)`);
   })
 
   it('#SolwitTeam should contain right text',()=>{
@@ -49,10 +49,11 @@ describe('Testing careera tab', () => {
     onCareerPage.toFindOutMoreAboutWorking()
   })
 
-  it.only('Check redirection to Facebook/Instagram/LinkedIn pages',()=>{
+  it('Check redirection to Facebook/Instagram/LinkedIn pages',()=>{
     onCareerPage.checkRedirectionToFacebookPage()
     onCareerPage.checkRedirectionToInstagramPage()
     onCareerPage.checkRedirectionToLinkedInPage()
   })
+
 
 })
