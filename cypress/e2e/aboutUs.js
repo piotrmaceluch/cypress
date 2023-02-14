9f7b588cdffc653ac83e0f039aebdcc3c1a7af79
tomekniemiec <tomek.niemiec@icloud.com>
2023-02-14 14:25:51 +0100

    fix assert

--- cypress/e2e/aboutUs.js
+++ cypress/e2e/aboutUs.js
@@ -13,39 +13,39 @@ Cypress.once('uncaught:exception', (err, runnable) => {
     
     it('should see "about us" tab', () => {
         onMainPage.navigateToTab("ABOUT US +","About us")
-        cy.contains('h1','About us')
+        cy.contains('h1','About us').should('be.visible')
     })
     
     it('should see "management" tab', () => {
         onMainPage.navigateToTab("ABOUT US +", "Management")
-        cy.contains('h2','Management')
+        cy.contains('h2','Management').should('be.visible')
     })
 
     it('should see "culture and policies" tab', () => {
         onMainPage.navigateToTab("ABOUT US +", "Culture and policies")
-        cy.contains('h1','Culture and Policies')
+        cy.contains('h1','Culture and Policies').should('be.visible')
     })
 
     it('should see "partnerships" tab', () => {
         onMainPage.navigateToTab("ABOUT US +", "Partnerships")
-        cy.contains('h2','PARTNERSHIPS')
+        cy.contains('h2','PARTNERSHIPS').should('be.visible')
     })
 
     it('should see "solwits social responsibility" tab', () => {
         onMainPage.navigateToTab("ABOUT US +", "Social Responsibility")
-        cy.contains('h1','Solwit’s social responsibility')
+        cy.contains('h1','Solwit’s social responsibility').should('be.visible')
     })
 
     it('should see "solwit group" tab', () => {
         onMainPage.navigateToTab("ABOUT US +", "Solwit Group")
-        cy.contains('h2','Solwit S.A. subsidiary')
+        cy.contains('h2','Solwit S.A. subsidiary').should('be.visible')
     })
 
     it('should see correct content about CEO', () => {
         onMainPage.navigateToTab("ABOUT US +", "Management")
         cy.get('.icon-right-arrow2').first().click()
-        cy.contains('h1','Leszek Pankiewicz')
-        cy.contains('p','A graduate of the Faculty of Electronics')
+        cy.contains('h1','Leszek Pankiewicz').should('be.visible')
+        cy.contains('p','A graduate of the Faculty of Electronics').should('be.visible')
     })
 
   })
---
+++ cypress/support/page_objects/blogPage.js
@@ -0,0 +1,33 @@
+import { BasePage } from "./basePage";
+
+export class BlogPage extends BasePage {
+    visit() {
+        cy.visit("https://solwit.com/en/blog/")
+    }
+
+    checkTitles() {
+        cy.title().should('eq', 'Blog – Solwit')
+        cy.contains(`div[class='elementor-widget-container'] h1[class='elementor-heading-title elementor-size-default']`, 'Welcome to our Blog!')
+    }
+
+    checkAllFilterCategory() {
+        this.#checkFilterCategory('Cloud services', 'tag-cloud-services-en')
+        this.#checkFilterCategory('IoT Solutions', 'tag-iot-solutions-en')
+        this.#checkFilterCategory('News', 'tag-news-en')
+        this.#checkFilterCategory('Outsourcing', 'tag-outsourcing-en')
+        this.#checkFilterCategory('Software development', 'tag-software-development')
+        this.#checkFilterCategory('Solwit Team', 'tag-solwit-team-en')
+        this.#checkFilterCategory('Tests', 'tag-tests-en-2')
+    }
+
+    #checkFilterCategory(category, className) {
+        cy.intercept('GET', '/en/blog/*').as('blog')
+        cy.get(`[for|='sf-input']`).contains(category).click()
+        cy.wait('@blog')
+        cy.get(`[class$=${className}]`)
+            .invoke('attr', 'class')
+            .should('contain', 'elementor-post elementor-grid-item')
+            .should('have.length.greaterThan', 1)
+    }
+}
+export const onBlogPage = new BlogPage()
